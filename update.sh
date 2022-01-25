e4b783935bc0f9815d7b96434380b7a97113c604
Metzger100 <62517154+metzger100@users.noreply.github.com>
2022-01-25 14:07:40 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #INSTALLING NEEDED PACKAGES AND CLEANING UP
-sudo apt update
-sudo apt -y install screen curl jq wget zip openjdk-17-jdk
-sudo apt -y autoremove
+sudo apt-get update
+sudo apt-get -y install screen curl jq wget zip openjdk-17-jdk
+sudo apt-get -y autoremove
 
 #STARTING NOIP2-SERVICE (REMOVE "#" IF YOU WANT TO USE)
 
