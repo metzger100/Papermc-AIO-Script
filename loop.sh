4a34225f20425e45162dc367676b092d9bfd502b
Metzger100 <62517154+metzger100@users.noreply.github.com>
2022-01-25 13:35:15 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-BINDIR=$(dirname "$(readlink -fn "$0")")
-cd "$BINDIR"
-
 echo "\033]0;minecraft\007"
 
 #STARTING LOOP
@@ -10,17 +7,15 @@ while true
 do
 
 #EULA-CHECK
-EULA="$BINDIR"/eula.txt
+EULA=eula.txt
 if [ -f "$EULA" ]; then
  echo 'ERROR: Eula already exists. If Error appears set eula "true" by hand.'
 else
- cd "$BINDIR"
  sh -c 'echo "eula=true" >> eula.txt'
  echo 'Eula accepted!'
 fi
 
 #PAPER-UPDATE (CHANGE version_group TO USE ANOTHER VERSION OF MINECRAFT (e.g: version_group=1.16); DON'T CHANGE WHEN WORLD IS ALREADY CREATED)
-cd "$BINDIR"
 rm server.jar
 
 version_group=1.18
@@ -45,20 +40,19 @@ chown $USER:$USER server.jar
 #BACKUP (TO BACKUP MORE LEVELS ADD THE NAME OF THE WORLD-LEVEL-FOLDER BEHIND "world_the_end" IN QOUTES)
 echo 'Backup starts! This can take a view minutes!'
 
-DIR="$BINDIR/backup/"
+DIR="backup/"
 
 if [ -d "$DIR" ]; then
-rm -r $BINDIR/backup
+rm -r backup
 echo 'Old Backup removed!'
 fi
 
-mkdir $BINDIR/backup/
-zip -r "$BINDIR/backup/backup.zip" "world" "world_nether" "world_the_end"
+mkdir backup/
+zip -r "backup/backup.zip" "world" "world_nether" "world_the_end"
 
 echo 'New Backup saved in /backup/!'
 
 #SERVERSTART (CHANGE Xms AND Xmx TO INCREASE/DECREASE MEMORY USAGE)
-cd "$BINDIR"
 java -Xms2G -Xmx2500M -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:-OmitStackTraceInFastThrow -XX:+ShowCodeDetailsInExceptionMessages -XX:+DisableExplicitGC -XX:-UseParallelGC -XX:+PerfDisableSharedMem -XX:+UseZGC -XX:-ZUncommit -XX:ZUncommitDelay=300 -XX:ZCollectionInterval=5 -XX:ZAllocationSpikeTolerance=2.0 -XX:+ExitOnOutOfMemoryError -XX:+AlwaysPreTouch -XX:-DontCompileHugeMethods -XX:+TrustFinalNonStaticFields -XX:+UseFastUnorderedTimeStamps -XX:+UseTransparentHugePages -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -XX:+UseCMoveUnconditionally -XX:+UseNewLongLShift -XX:+UseVectorCmov -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+ParallelRefProcEnabled -jar server.jar nogui
 
 echo 'Want to stop Server? Press ctrl + c in the next 5 seconds!'
