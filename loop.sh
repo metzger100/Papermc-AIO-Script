6f0e05877b77a0fffa0ada0c3e07c2015a644d12
Metzger100 <62517154+metzger100@users.noreply.github.com>
2022-02-09 03:44:00 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -51,7 +51,7 @@ zip -r "backup/backup.zip" "world" "world_nether" "world_the_end" "plugins"
 #PASSWD='yourpassword'
 #UPLOADDIR='yourpathforthehost(Folders must already exist)'
 #FILE='backup/backup.zip'
-#lftp -u $USER,$PASSWD -e "put -O $UPLOADDIR $FILE;quit" $HOST
+#lftp -u $USER,$PASSWD -e "set net:max-retries 2; put -O $UPLOADDIR $FILE;quit" $HOST
 
 #NEXTCLOUD - TO USE IT UNCOMMENT IT AND ENTER THE 3 PROPER SPECIFICATIONSIN THE FOLLOWING 3 LINES(https://docs.nextcloud.com/server/18/user_manual/files/access_webdav.html#accessing-files-using-curl)
 #HOST='yourhost e.g. https://nextcloud.mydomain.tld/remote.php/dav/files/scannerusername/ScansnapInbox/$filename'
