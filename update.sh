8ef6d33a158cbe0ef4c2f46bef2d62e9ebae11ef
Metzger100 <62517154+metzger100@users.noreply.github.com>
2023-12-01 09:51:47 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,4 +7,4 @@ apt-get -y install screen curl jq wget zip openjdk-17-jdk lftp
 apt-get -y autoremove
 
 #STARTING NOIP2-SERVICE (REMOVE "#" IF YOU WANT TO USE)
-/usr/local/bin/noip2
+#/usr/local/bin/noip2
